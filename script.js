98f68070631ceb042bd88cfa78cc8a911360dc18
Julio Lucas <julio.lucas@gmail.com>
2020-02-12 16:36:28 +0000

    ver 4

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
+// ! constantes para selecionar os ID´s
 const PONTEIRO_HORAS = document.querySelector('#hour');
 const PONTEIRO_MINUTOS = document.querySelector('#minute');
 const PONTEIRO_SEGUNDOS = document.querySelector('#second');
 
 
-//! object data
-var date = new Date();
-console.log(date);
+//-----------------------------------------------------------------------------------------------------
+
+// ! Função para ativar o relogio
+
+function runTheClock() {
+
+    //! Object data
+    var date = new Date();
+    console.log(date);
 
 
-//! obteção da hora, minutos e segundos
-let hr = date.getHours();
-let min = date.getMinutes();
-let sec = date.getSeconds();
-console.log('Horas: ' + hr + 'Minutos: ' + min + 'Segundos: ' + sec);
+    //! Obteção da hora, minutos e segundos
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    console.log('Horas: ' + hr + 'Minutos: ' + min + 'Segundos: ' + sec);
 
-//! posição em graus
-// formula para o movimento (ser mais suave) do ponteiro das horas e acompanhar o dos minutos
-let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
-// formula para o movimento (ser mais suave) do ponteiro dos minutos e acompanhar o dos segundos
-let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
-let secPosition = sec * 360 / 60;
+    //! Posição em graus
+    // formula para o movimento (ser mais suave) do ponteiro das horas e acompanhar o dos minutos
+    let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
+    // formula para o movimento (ser mais suave) do ponteiro dos minutos e acompanhar o dos segundos
+    let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
+    let secPosition = sec * 360 / 60;
 
 
-//! transformação dos graus em posição grafica
-PONTEIRO_HORAS.style.transform = 'rotate(' + hrPosition + 'deg)';
-PONTEIRO_MINUTOS.style.transform = 'rotate(' + minPosition + 'deg)';
-PONTEIRO_SEGUNDOS.style.transform = 'rotate(' + secPosition + 'deg)';
+    //! Transformação dos graus em posição grafica
+    PONTEIRO_HORAS.style.transform = 'rotate(' + hrPosition + 'deg)';
+    PONTEIRO_MINUTOS.style.transform = 'rotate(' + minPosition + 'deg)';
+    PONTEIRO_SEGUNDOS.style.transform = 'rotate(' + secPosition + 'deg)';
 
+}
+
+//-----------------------------------------------------------------------------------------------------
+
+// ! Intervalo de tempo
+// corre o relogio a cada segundo
+var interval = setInterval(runTheClock, 1000);
 
 
 
