845b1146263e7770f0584988ed45f8ec1799c6d7
Julio Lucas <julio.lucas@gmail.com>
2020-02-12 16:22:13 +0000

    ver 3

--- script.js
+++ script.js
@@ -3,25 +3,26 @@ const PONTEIRO_MINUTOS = document.querySelector('#minute');
 const PONTEIRO_SEGUNDOS = document.querySelector('#second');
 
 
-// object data
+//! object data
 var date = new Date();
 console.log(date);
 
 
-// obteção da hora, minutos e segundos
+//! obteção da hora, minutos e segundos
 let hr = date.getHours();
 let min = date.getMinutes();
 let sec = date.getSeconds();
 console.log('Horas: ' + hr + 'Minutos: ' + min + 'Segundos: ' + sec);
 
-// posição em graus
-let hrPosition = 20;
+//! posição em graus
+// formula para o movimento (ser mais suave) do ponteiro das horas e acompanhar o dos minutos
+let hrPosition = (hr * 360 / 12) + (min * (360 / 60) / 12);
 // formula para o movimento (ser mais suave) do ponteiro dos minutos e acompanhar o dos segundos
 let minPosition = (min * 360 / 60) + (sec * (360 / 60) / 60);
 let secPosition = sec * 360 / 60;
 
 
-// transformação dos graus em posição grafica
+//! transformação dos graus em posição grafica
 PONTEIRO_HORAS.style.transform = 'rotate(' + hrPosition + 'deg)';
 PONTEIRO_MINUTOS.style.transform = 'rotate(' + minPosition + 'deg)';
 PONTEIRO_SEGUNDOS.style.transform = 'rotate(' + secPosition + 'deg)';
